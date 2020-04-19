0a2ebd6c54237f740d3440d6b7844eef32547490
Klausstaler <fredrik.klauss@gmx.de>
2020-04-19 17:49:46 +0200

    Refactor scraper

--- Redditscraper.py
+++ Redditscraper.py
@@ -1,7 +1,7 @@
 import praw, os, pickle, datetime, re
 from praw.models import MoreComments
 from Screenshotter import Screenshotter
-import time
+from enum import Enum
 
 CLIENT_ID = os.getenv("CLIENT_ID_REDDIT")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET_REDDIT")
@@ -15,46 +15,83 @@ reddit = praw.Reddit(client_id=CLIENT_ID,
                      client_secret=CLIENT_SECRET,
                      user_agent=USER_AGENT)
 
+class TimeFilter(Enum):
+    DAY = "day"
+    WEEK = "week"
+    MONTH = "month"
+    YEAR = "year"
+    ALL = "all"
+class SortMethod(Enum):
+    NEW = "new"
+    BEST = "confidence"
+    TOP = "top"
+    CONTROVERSIAL = "controversial"
+    OLD = "old"
+    QA = "qa"
+
+def _check_text(text):
+    for word in ["shoot", "shit", "fuck", "nigg", "massacre"]:
+        if word in text.lower():
+            return False
+    return True
+
+
+def _clean_str(text):
+    text = re.sub('https*://[\w\.]+\.com[\w/\-]+|https*://[\w\.]+\.com|[\w\.]+\.com/[\w/\-]+',
+                  lambda x: re.findall('(?<=\://)[\w\.]+\.com|[\w\.]+\.com', x.group())[0] + " link", text)
+    new_text = []
+    for i, char in enumerate(text):
+        if char == "\n" or char == "\t":
+            new_text.append(".")
+        elif char not in ["*", "^", "\\", "\"", "<", ">", "[", "]"]:
+            new_text.append(char)
+    return "".join(new_text)
+
 
 class Subreddit:
     def __init__(self, subreddit):
         self.subreddit = subreddit
         self.sub = reddit.subreddit(subreddit)
+        with open("visited.txt", "r") as f:
+            self.visited = set([ID.strip() for ID in f.readlines()])
 
     def get_top(self, n, timefilter):
-        if timefilter not in ["day", "week", "month", "year", "all"]:
-            raise ValueError("Wrong time filter! Set it to day, week, month, year or \"all\"")
+        timefilter = TimeFilter(timefilter)
         with open("visited.txt", "r") as f:
-            visited = set([ID.strip() for ID in f.readlines()])
-        top = self.sub.top(limit=n, time_filter=timefilter)
-        for post in top:
-            print("POSTING")
-            if post.id not in visited and self._check_text(post.title):
-                self.sc = Screenshotter(f"https://reddit.com/r/{self.subreddit}/comments/{post.id}/?sort=top", post.id)
-                print("Fetching comments...")
-                post.comments.replace_more(limit=125)
-                print("Comments fetched!")
-                self._create_instr(post)
-                visited.add(post.id)
-                with open("visited.txt", "a") as f:
-                    f.write(f"{post.id}\n")
-                    f.close()
+            self.visited = set([ID.strip() for ID in f.readlines()])
+        top = self.sub.top(limit=n, time_filter=timefilter.value)
+        i = 0
+        while i < len(top):
+            post = top[i]
+            if post.id in self.visited:
+                del top[i]
+            else:
+                i += 1
+        return top
 
-    def _check_text(self, text):
-        for word in ["shoot", "shit", "fuck", "nigg", "massacre"]:
-            if word in text.lower():
-                return False
-        return True
+    def create_screenshots(self, post, order):
+        order = SortMethod(order)
+        if post.id not in self.visited and _check_text(post.title):
+            self.sc = Screenshotter(f"https://reddit.com/r/{self.subreddit}/comments/{post.id}/?sort={order.value}", post.id)
+            print("Fetching comments...")
+            post.comments.replace_more(limit=125)
+            print("Comments fetched!")
+            path = self._create_instr(post)
+            self.visited.add(post.id)
+            with open("visited.txt", "a") as f:
+                f.write(f"{post.id}\n")
+            return path
 
     def _create_instr(self, post):
         res = [[post.id, post.title]]
         self.sc.screenshot_title(f"tmp/screenshots/{post.id}")
         self._create_instr_help(post.comments, 0, res, 1)
-        time = datetime.datetime.now()
-        f = open(f"threads/{post}_{time.day}_{time.month}_{time.year}.pkl", "wb")
+        t = datetime.datetime.now()
+        path = f"threads/{post}_{t.day}_{t.month}_{t.year}.pkl"
+        f = open(path, "wb")
         pickle.dump(res, f)
         f.close()
-        # print(res)
+        return path[8:]
 
     def _create_instr_help(self, comments, prevScore, instructions, count):
         """
@@ -64,10 +101,10 @@ class Subreddit:
             if isinstance(comment, MoreComments):
                 continue
             if comment.score >= max(20, prevScore * 0.2) and comment.body not in ["[deleted]",
-                                                                                  "[removed]"] and self._check_text(
-                    comment.body):
+                                                                                  "[removed]"] and _check_text(
+                comment.body):
                 instructions.append([NEW_COMMENT if not prevScore else SUB_COMMENT, ""])
-                text = self._clean_str(comment.body.strip())
+                text = _clean_str(comment.body.strip())
                 instructions.append([comment.id, text])
                 print("NEW_COMMENT" if not prevScore else "SUB_COMMENT", text, comment.id)
                 self.sc.screenshot_comment(comment.id, f"tmp/screenshots/{comment.id}")
@@ -77,13 +114,3 @@ class Subreddit:
                 # self.sc.driver.back()
                 # time.sleep(1)
 
-    def _clean_str(self, text):
-        text = re.sub('https*://[\w\.]+\.com[\w/\-]+|https*://[\w\.]+\.com|[\w\.]+\.com/[\w/\-]+',
-                      lambda x: re.findall('(?<=\://)[\w\.]+\.com|[\w\.]+\.com', x.group())[0] + " link", text)
-        new_text = []
-        for i, char in enumerate(text):
-            if char == "\n" or char == "\t":
-                new_text.append(".")
-            elif char not in ["*", "^", "\\", "\"", "<", ">", "[", "]"]:
-                new_text.append(char)
-        return "".join(new_text)
--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import os
 
 if __name__ == "__main__":
     #get_top("askreddit", 50, "all")
-    #sub = Subreddit("askreddit")
-    #sub.get_top(5, "all")
-    paths = os.listdir("threads/")
-    for path in paths:
+    sub = Subreddit("askreddit")
+    top = sub.get_top(5, "all")
+    for post in top:
+        path = sub.create_screenshots(post, "top")
         video_path = create_video("threads/" + path, path.split("_")[0])
         #os.system(
         #    f"python Upload.py --file=\"{video_path}\" --title=\"Test\" --description=\"I am the best\" --keywords=\"Reddit, AskReddit\" --privacyStatus=\"private\"")
