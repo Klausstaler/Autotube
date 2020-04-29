bf2b6aedbe47bb2fed86ef4aae7d145a20de86c1
Klausstaler <fredrik.klauss@gmx.de>
2020-04-29 17:15:55 +0200

    Modify main for new string slicing

--- main.py
+++ main.py
@@ -16,15 +16,15 @@ if __name__ == "__main__":
     print("Creating video setup...")
     video_setup = VideoSetup(20, 25, 70)
     print("Video setup is set up!")
-    top = sub.get_top(5, "all")
+    top = sub.get_top(100, "all")
     for post in top:
+        print(f"Working on {post.title}")
         try:
             path = sub.create_screenshots(post, "top")
         except UnsuitableThreadErr as e:
             continue
-        video_path = video_setup.create_video("threads/" + path, path.split("_")[0])
+        video_path = video_setup.create_video(path, path.split("/")[-1].split("_")[0])
         # os.remove(f"threads/{path}")
         # os.system(
         #    f"python Upload.py --file=\"{video_path}\" --title=\"Test\" --description=\"I am the best\" --keywords=\"Reddit, AskReddit\" --privacyStatus=\"private\"")
-        break
         # os.system(f"python Upload.py --file=\"{video_path}\" --title=\"Test\" --description=\"I am the best\" --keywords=\"Reddit, AskReddit\" --privacyStatus=\"private\"")
